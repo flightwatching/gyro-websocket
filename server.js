826a436a5172ef2fb7a6540ab314dbcfa4184f22
dao hodac <dao.hodac@gmail.com>
2016-05-03 15:53:30 +0200

    add a simulator WS room

--- server.js
+++ server.js
@@ -15,18 +15,44 @@ app.get('/client.js', function(req, res){
   res.sendfile('client.js');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-  socket.on('touch', function(msg){
-    io.emit('touch', msg);
-  });
-  // socket.on('devicemotion', function(msg){
-  //   console.log(JSON.parse(msg));
-  // });
-  socket.on('deviceorientation', function(msg){
-    io.emit('deviceorientation', msg);
+var started=false;
+var simuStarted = false;
+var status = {connectCounter:0, i:0};
+
+function generateSimu() {
+  if (!simuStarted)
+    return;
+  var val = JSON.stringify(status);
+  status.i++;
+  io.emit('simu', val);
+  var generateSimuThread = setTimeout(generateSimu,1000);
+}
+
+
+
+io.on('connection', function(socket) {
+  status.connectCounter++;
+  console.info ("we have "+status.connectCounter+" users")
+  if (!started) {
+    socket.on('touch', function(msg){
+      io.emit('touch', msg);
+    });
+    socket.on('deviceorientation', function(msg){
+      io.emit('deviceorientation', msg);
+    });
+    started = true;
+  }
+  if (!simuStarted) {
+    console.info("starting simu because we have users");
+    simuStarted=true;
+    generateSimu();
+  }
+  socket.on('disconnect', function() {
+    status.connectCounter--;
+    if (status.connectCounter<=0) {
+      console.info("Stopping simu because no more users");
+      simuStarted=false;
+    }
   });
 });
 
